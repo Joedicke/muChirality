e3873b2f38c882aeb5d0fddd87eb892b7b6314e0
joedicke <indre.joedicke@imtek.uni-freiburg.de>
2023-12-22 16:02:30 +0100

    Add EigenStrain class for torsion problems

---
+++ muChirality/EigenStrainTorsion.py
@@ -0,0 +1,173 @@
+"""
+@file   EigenStrainTorsion.py
+
+@author Indre  Joedicke <indre.joedicke@imtek.uni-freiburg.de>
+
+@date   18 Jul 2023
+
+@brief  Class for eigenstrains due to loading with twist
+
+Copyright © 2018 Till Junge
+
+µSpectre is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public License as
+published by the Free Software Foundation, either version 3, or (at
+your option) any later version.
+
+µSpectre is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with µSpectre; see the file COPYING. If not, write to the
+Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+Boston, MA 02111-1307, USA.
+
+Additional permission under GNU GPL version 3 section 7
+
+If you modify this Program, or any covered work, by linking or combining it
+with proprietary FFT implementations or numerical libraries, containing parts
+covered by the terms of those libraries' licenses, the licensors of this
+Program grant you additional permission to convey the resulting work.
+"""
+import numpy as np
+
+class EigenStrain:
+    """
+    Class for describing the eigenstrain due to imposed torsion along z-axis
+    for a five tetrahedra discretization.
+
+    Attributes
+    ----------
+    nb_grid_pts: list of 3 ints
+                 Number of grid pts in each direction
+    pixels: µSpectre pixels object
+            pixels of the discretized unit cell
+    angle: float
+           imposed rotation angle
+    hx: float
+        voxel length in x-direction
+    hy: float
+        voxel length in y-direction
+    x_rot_axis: float
+                x-coordinate of the rotation axis
+    y_rot_axis: float
+                y-coordinate of the rotation axis
+
+    Methods
+    -------
+    eigen_strain_func(step_nb, strain_field):
+          Change the strain_field to account for the eigen strain.
+    remove_eigen_strain_func(strain_field):
+          Inverse of eigen_strain_func.
+    """
+    def __init__(self, pixels, angle, lengths, nb_grid_pts, x_rot_axis, y_rot_axis):
+        """
+        Parameters
+        ----------
+        pixels: µSpectre pixels object
+                pixels of the discretized unit cell
+        angle: float
+               imposed rotation angle
+        lengths: List of 3 floats
+                 Lengths of the unit cell in cartesian directions
+        nb_grid_pts: List of 3 ints
+             Number of voxels in cartesian directions
+        x_rot_axis: float
+             x-coordinate of the rotation axis
+        y_rot_axis: float
+             y-coordinate of the rotation axis
+        """
+        self.nb_grid_pts = nb_grid_pts
+        self.pixels = pixels
+        self.angle = angle
+        self.hx = lengths[0] / nb_grid_pts[0]
+        self.hy = lengths[1] / nb_grid_pts[1]
+        self.x_rot_axis = x_rot_axis
+        self.y_rot_axis = y_rot_axis
+
+    def __call__(self, nb_steps, strain_field):
+        self.eigen_strain_func(step_nb, strain_field)
+
+    def eigen_strain_func(self, step_nb, strain_field):
+        """
+        Change the strain_field to account for the eigen strain.
+
+        Parameters
+        ----------
+        step_nb: int
+                 Number of load step
+        strain_field: np.array(3, 3, 5, *nb_grid_pts) of floats
+                      Strain field
+        """
+        hx = self.hx
+        hy = self.hy
+        x = np.arange(self.nb_grid_pts[0]) * hx # x-coordinate of voxel
+        y = np.arange(self.nb_grid_pts[1]) * hy # y-coordinate of voxel
+        x_rot_axis = self.x_rot_axis
+        y_rot_axis = self.y_rot_axis
+        angle = self.angle
+
+        # x-y-coordinates of the centers of the 5 tetradras
+        X, Y = np.meshgrid(x, y)
+        X = X.transpose() # Format X[i, j, k] with i: quad_pt, j: x-coord, k: y-coord
+        Y = Y.transpose()
+        X = np.stack((X, X, X, X, X))
+        Y = np.stack((Y, Y, Y, Y, Y))
+        X[0] += 0.5 * hx
+        X[1] += 0.25 * hx
+        X[2] += 0.75 * hx
+        X[3] += 0.75 * hx
+        X[4] += 0.25 * hx
+        Y[0] += 0.5 * hy
+        Y[1] += 0.25 * hy
+        Y[2] += 0.75 * hy
+        Y[3] += 0.25 * hy
+        Y[4] += 0.75 * hy
+
+        # Eigenstrain
+        strain_field[0, 2] -= 0.5 * angle * (Y[:, :, :, None] - y_rot_axis)
+        strain_field[2, 0] -= 0.5 * angle * (Y[:, :, :, None] - y_rot_axis)
+        strain_field[1, 2] += 0.5 * angle * (X[:, :, :, None] - x_rot_axis)
+        strain_field[2, 1] += 0.5 * angle * (X[:, :, :, None] - x_rot_axis)
+
+    def remove_eigen_strain_func(self, strain_field):
+        """
+        Change the strain_field to remove the influence of the eigen strain.
+
+        Parameters
+        ----------
+        strain_field: np.array(3, 3, 5, *nb_grid_pts) of floats
+                      Strain field
+        """
+        hx = self.hx
+        hy = self.hy
+        x = np.arange(self.nb_grid_pts[0]) * hx # x-coordinate of voxel
+        y = np.arange(self.nb_grid_pts[1]) * hy # y-coordinate of voxel
+        x_rot_axis = self.x_rot_axis
+        y_rot_axis = self.y_rot_axis
+        angle = self.angle
+
+        # x-y-coordinates of the centers of the 5 tetradras
+        X, Y = np.meshgrid(x, y)
+        X = X.transpose() # Format X[i, j, k] with i: quad_pt, j: x-coord, k: y-coord
+        Y = Y.transpose()
+        X = np.stack((X, X, X, X, X))
+        Y = np.stack((Y, Y, Y, Y, Y))
+        X[0] += 0.5 * hx
+        X[1] += 0.25 * hx
+        X[2] += 0.75 * hx
+        X[3] += 0.75 * hx
+        X[4] += 0.25 * hx
+        Y[0] += 0.5 * hy
+        Y[1] += 0.25 * hy
+        Y[2] += 0.75 * hy
+        Y[3] += 0.25 * hy
+        Y[4] += 0.75 * hy
+
+        # Remove eigenstrain
+        strain_field[0, 2] += 0.5 * angle * (Y[:, :, :, None] - y_rot_axis)
+        strain_field[2, 0] += 0.5 * angle * (Y[:, :, :, None] - y_rot_axis)
+        strain_field[1, 2] -= 0.5 * angle * (X[:, :, :, None] - x_rot_axis)
+        strain_field[2, 1] -= 0.5 * angle * (X[:, :, :, None] - x_rot_axis)
---
+++ test/test_eigenstrain.py
@@ -0,0 +1,138 @@
+"""
+Test the EigenStrain class for torsion problems
+"""
+
+import sys
+import os
+
+# Default path of the library
+sys.path.insert(0, os.path.join(os.getcwd(), "./muspectre/build/language_bindings/python"))
+sys.path.insert(0, os.path.join(os.getcwd(), "./muspectre/build/language_bindings/libmufft/python"))
+sys.path.insert(0, os.path.join(os.getcwd(), "./muspectre/build/language_bindings/libmugrid/python"))
+
+import numpy as np
+
+import muSpectre as µ
+
+from muChirality.EigenStrainTorsion import EigenStrain
+
+def test_eigen_strain():
+    """ Test wether the eigen strain is correctly added.
+    """
+    ### ----- Parameters ----- ###
+    nb_grid_pts = [6, 6, 6]
+    lengths = [1, 1, 10]
+    Young = 100
+    Poisson = 0
+    angle = 0.15
+
+    # Formulation
+    formulation = µ.Formulation.small_strain
+
+    # Numerical derivative (FE 5 Tetraeder + weights)
+    gradient, weights = µ.linear_finite_elements.gradient_3d_5tet
+
+    # muSpectre solver parameters
+    newton_tol       = 1e-7
+    cg_tol           = 1e-7 # tolerance for cg algo
+    equil_tol        = 1e-7 # tolerance for equilibrium
+    maxiter          = 10000
+    verbose          = µ.Verbosity.Silent
+
+    # Loading is pure torsion
+    delta_F = np.zeros((3, 3))
+    x_torsion = lengths[0] / 2
+    y_torsion = lengths[1] / 2
+
+    ### ----- Initialization of µSpectre ----- ###
+    cell = µ.Cell(nb_grid_pts, lengths, formulation, gradient, weights)
+    solver = µ.solvers.KrylovSolverCG(cell, cg_tol, maxiter, verbose)
+
+    # Eigen class
+    eigen_class = EigenStrain(cell.pixels, angle, lengths, nb_grid_pts,
+                              x_torsion, y_torsion)
+
+    ### ----- Test eigen strain ----- ###
+    strain = np.random.random((3, 3, cell.nb_quad_pts, *nb_grid_pts))
+    strain_new = strain.copy()
+    eigen_class.eigen_strain_func(0, strain_new)
+
+    hx = lengths[0] / nb_grid_pts[0]
+    hy = lengths[1] / nb_grid_pts[1]
+    hz = lengths[2] / nb_grid_pts[2]
+
+    # Test the strain at one pixel, quadrature point 0
+    ind_x, ind_y, ind_z = [0, 3, 3]
+    strain_twist_01 = - 0.5 * angle * (ind_y * hy + 0.5 * hy - y_torsion)
+    strain_twist_02 = 0.5 * angle * (ind_x * hx + 0.5 * hx - x_torsion)
+    strain_twist = np.array([[0, 0, strain_twist_01],
+                             [0, 0, strain_twist_02],
+                             [strain_twist_01, strain_twist_02, 0]])
+
+    diff = strain_new[:, :, 0, ind_x, ind_y, ind_z]
+    diff = diff - strain[:, :, 0, ind_x, ind_y, ind_z] - strain_twist
+    diff = np.linalg.norm(diff)
+    assert diff < 1e-10
+
+    # Test the strain at other pixel, quadrature point 1
+    ind_x, ind_y, ind_z = [2, 2, -1]
+    strain_twist_01 = - 0.5 * angle * (ind_y * hy + 0.25 * hy - y_torsion)
+    strain_twist_02 = 0.5 * angle * (ind_x * hx + 0.25 * hx - x_torsion)
+    strain_twist = np.array([[0, 0, strain_twist_01],
+                             [0, 0, strain_twist_02],
+                             [strain_twist_01, strain_twist_02, 0]])
+
+    diff = strain_new[:, :, 1, ind_x, ind_y, ind_z]
+    diff = diff - strain[:, :, 1, ind_x, ind_y, ind_z] - strain_twist
+    diff = np.linalg.norm(diff)
+    assert diff < 1e-10
+
+def test_remove_eigen_strain():
+    """ Test wether the eigen strain can be correctly removed.
+    """
+    ### ----- Parameters ----- ###
+    nb_grid_pts = [4, 4, 4]
+    lengths = [1, 1, 10]
+    Young = 100
+    Poisson = 0
+    angle = 0.15
+
+    # Formulation
+    formulation = µ.Formulation.small_strain
+
+    # Numerical derivative (FE 5 Tetraeder + weights)
+    gradient, weights = µ.linear_finite_elements.gradient_3d_5tet
+
+    # muSpectre solver parameters
+    newton_tol       = 1e-7
+    cg_tol           = 1e-7 # tolerance for cg algo
+    equil_tol        = 1e-7 # tolerance for equilibrium
+    maxiter          = 10000
+    verbose          = µ.Verbosity.Silent
+
+    # Loading is pure torsion
+    delta_F = np.zeros((3, 3))
+    x_torsion = lengths[0] / 2
+    y_torsion = lengths[1] / 2
+
+    ### ----- Initialization of µSpectre ----- ###
+    cell = µ.Cell(nb_grid_pts, lengths, formulation, gradient, weights)
+    solver = µ.solvers.KrylovSolverCG(cell, cg_tol, maxiter, verbose)
+
+    # Eigen class
+    eigen_class = EigenStrain(cell.pixels, angle, lengths, nb_grid_pts,
+                              x_torsion, y_torsion)
+
+    ### ----- Testing ----- ###
+    strain = np.random.random((3, 3, cell.nb_quad_pts, *nb_grid_pts))
+    strain_new = strain.copy()
+
+    # eigen_strain_func changes the strain
+    eigen_class.eigen_strain_func(0, strain_new)
+    diff = np.linalg.norm(strain - strain_new)
+    assert(diff > 1e-10)
+
+    # remove_eigen_strain_func should remove the change
+    eigen_class.remove_eigen_strain_func(strain_new)
+    diff = np.linalg.norm(strain - strain_new)
+    assert(diff < 1e-10)
